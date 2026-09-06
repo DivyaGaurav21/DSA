4edab36b5e48588f69631ec88ce92798706fefee
Divya Gaurav <104293206+DivyaGaurav21@users.noreply.github.com>
2026-09-06 13:13:42 -0700

    Time: 1 ms (88.99%), Space: 54.4 MB (73.92%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,12 +3,28 @@
  * @param {number} target
  * @return {number[]}
  */
+
+ //Brute
+// var twoSum = function(nums, target) {
+//     for(let i = 0; i < nums.length; i++){
+//         for(let j = i + 1; j < nums.length; j++){
+//               if(nums[i] + nums[j] === target){
+//                 return [i , j];
+//               }
+//         }
+//     }
+// };
+//HashTable
 var twoSum = function(nums, target) {
-    for(let i = 0; i < nums.length; i++){
-        for(let j = i + 1; j < nums.length; j++){
-              if(nums[i] + nums[j] === target){
-                return [i , j];
-              }
+    const map = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+
+        if (map.has(complement)) {
+            return [map.get(complement), i];
         }
+
+        map.set(nums[i], i);
     }
 };
