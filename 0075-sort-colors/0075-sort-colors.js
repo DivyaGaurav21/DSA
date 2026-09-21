be8c1d84fb54ad8580953f7d34917bb5e5c6ea7c
Divya Gaurav <104293206+DivyaGaurav21@users.noreply.github.com>
2026-09-21 04:20:51 -0700

    Time: 0 ms (100%), Space: 52.9 MB (91.14%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -2,26 +2,44 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
+// var sortColors = function(nums) {
+//    let zeroCount = 0;
+//    let oneCount = 0;
+//    let twoCount = 0;
+//    for(let i = 0; i < nums.length; i++){
+//     if(nums[i] === 0){
+//         zeroCount ++;
+//     }else if(nums[i] === 1){
+//         oneCount ++;
+//     }else{
+//         twoCount ++;
+//     }
+//    }
+//     for(let i = 0; i < zeroCount; i++){
+//         nums[i] = 0;
+//     }
+//     for(let i = zeroCount; i < zeroCount + oneCount; i++){
+//         nums[i] = 1;
+//     }
+//     for(let i = zeroCount + oneCount; i < nums.length; i++){
+//         nums[i] = 2;
+//     }
+// };
+
 var sortColors = function(nums) {
-   let zeroCount = 0;
-   let oneCount = 0;
-   let twoCount = 0;
-   for(let i = 0; i < nums.length; i++){
-    if(nums[i] === 0){
-        zeroCount ++;
-    }else if(nums[i] === 1){
-        oneCount ++;
-    }else{
-        twoCount ++;
+    let low = 0, mid = 0, high = nums.length - 1;
+    while(mid <= high){
+        if(nums[mid] === 0){
+            nums[mid] = nums[low];
+            nums[low] = 0;
+            mid ++;
+            low ++; 
+        }else if(nums[mid] === 1){
+            mid ++;
+        }else{
+            nums[mid] = nums[high];
+            nums[high] = 2;
+            high --;
+        }
     }
-   }
-    for(let i = 0; i < zeroCount; i++){
-        nums[i] = 0;
-    }
-    for(let i = zeroCount; i < zeroCount + oneCount; i++){
-        nums[i] = 1;
-    }
-    for(let i = zeroCount + oneCount; i < nums.length; i++){
-        nums[i] = 2;
-    }
-};
+}
